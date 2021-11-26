0fa0fe842aa8d9bb19598a279578e1fab8a954bd
Ido <idoja@gmail.com>
2021-11-27 00:30:36 +0200

    after remove package Microsoft.AspNetCore.Mvc.WebApiCompatShim.

--- ParkingGarage/Controllers/Vehicles/VehiclesController.cs
+++ ParkingGarage/Controllers/Vehicles/VehiclesController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
