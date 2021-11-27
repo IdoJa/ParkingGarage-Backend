c3e18de2546e6070ee79e811d7b2030dc28edbb4
Ido <idoja@gmail.com>
2021-11-27 17:07:54 +0200

    after implementing GetAllTicketsByVehicle on VehiclesLogic.cs

---
+++ ParkingGarage/BusinessLogic/Tickets/TicketsLogic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using ParkingGarage.BusinessLogic.ParkingLot;
+using Vehicle.Ticket;
+
+namespace ParkingGarage.BusinessLogic.Tickets
+{
+    public class TicketsLogic
+    {
+        private readonly ParkingLotsLogic _parkingLotsLogic;
+
+        public TicketsLogic(ParkingLotsLogic parkingLotsLogic)
+        {
+            _parkingLotsLogic = parkingLotsLogic;
+        }
+
+        public Ticket CreateTicket(string ticket)
+        {
+            Ticket returnValue = null;
+            switch (ticket)
+            {
+                case "Vip":
+                    returnValue = new Vip(_parkingLotsLogic);
+                    break;
+                case "Value":
+                    returnValue = new Value(_parkingLotsLogic);
+                    break;
+                case "Regular":
+                    returnValue = new Regular(_parkingLotsLogic);
+                    break;
+            }
+
+            return returnValue;
+        }
+
+        public List<Ticket> GetAllTickets()
+        {
+            return new List<Ticket>()
+            {
+                new Vip(_parkingLotsLogic),
+                new Value(_parkingLotsLogic),
+                new Regular(_parkingLotsLogic)
+            };
+        }
+        
+    }
+}
--- ParkingGarage/BusinessLogic/Vehicles/VehiclesLogic.cs
+++ ParkingGarage/BusinessLogic/Vehicles/VehiclesLogic.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.IIS;
 using ParkingGarage.BusinessLogic.ParkingLot;
+using ParkingGarage.BusinessLogic.Tickets;
 using ParkingGarage.Data.Vehicle;
 using ParkingGarage.Helpers.HttpStatusException;
+using Vehicle.Impl;
 using Vehicle.Ticket;
 
 namespace ParkingGarage.BusinessLogic
@@ -15,11 +18,13 @@ namespace ParkingGarage.BusinessLogic
     {
         private readonly IVehicleRepo _vehicleRepo;
         private readonly ParkingLotsLogic _parkingLotsLogic;
+        private readonly TicketsLogic _ticketsLogic;
 
-        public VehiclesLogic(IVehicleRepo vehicleRepo, ParkingLotsLogic parkingLotsLogic)
+        public VehiclesLogic(IVehicleRepo vehicleRepo, ParkingLotsLogic parkingLotsLogic, TicketsLogic ticketsLogic)
         {
             _vehicleRepo = vehicleRepo;
             _parkingLotsLogic = parkingLotsLogic;
+            _ticketsLogic = ticketsLogic;
         }
 
         public List<global::Vehicle.Vehicle> GetAllVehicles()
@@ -37,7 +42,7 @@ namespace ParkingGarage.BusinessLogic
         public void CreateVehicle(Vehicle.Vehicle vehicle)
         {
             // check vehicle class
-            var freeParkingLot = CreateTicket(vehicle.Ticket).GetFreeParkingLot();
+            var freeParkingLot = _ticketsLogic.CreateTicket(vehicle.Ticket).GetFreeParkingLot();
             if (freeParkingLot == null)
             {
                 // there is no free lot for this ticket type
@@ -48,25 +53,46 @@ namespace ParkingGarage.BusinessLogic
             _vehicleRepo.CreateVehicle(vehicle);
         }
 
-        public Ticket CreateTicket(string ticket)
+        /// <summary>
+        /// example: for <see cref="Van"/> the class is "B" check if match with ticket class "Regular"
+        /// in this case, no return. and there is no option to select "Regular"
+        /// </summary>
+        /// <param name="vehicle"></param>
+        /// <returns></returns>
+        public List<Ticket> GetAllTicketsByVehicle(Vehicle.Vehicle vehicle)
         {
-            Ticket returnValue = null;
-            switch (ticket)
+            // for every Vehicle check if its class matches with ticket class list - if no dont return it
+            return _ticketsLogic.GetAllTickets().Where(ticket => ticket.ClassList.Contains(vehicle.Class)).ToList();
+        }
+
+
+        // return valid ticket types based on selected vehicle type
+        private Vehicle.Vehicle CreateVehicle(string vehicle)
+        {
+            Vehicle.Vehicle returnValue = null;
+            switch (vehicle)
             {
-                case "Vip":
-                    returnValue = new Vip(_parkingLotsLogic);
+                case "Motorcycle":
+                    returnValue = new Motorcycle();
                     break;
-                case "Value":
-                    returnValue = new Value(_parkingLotsLogic);
+                case "Private":
+                    returnValue = new Private();
                     break;
-                case "Regular":
-                    returnValue = new Regular(_parkingLotsLogic);
+                case "Crossover":
+                    returnValue = new Crossover();
+                    break;
+                case "SUV":
+                    returnValue = new SUV();
+                    break;
+                case "Van":
+                    returnValue = new Van();
+                    break;
+                case "Truck":
+                    returnValue = new Truck();
                     break;
             }
 
             return returnValue;
         }
-        
-        // return valid ticket types based on selected vehicle type
     }
 }
